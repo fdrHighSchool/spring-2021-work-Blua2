2dc588c09ebda045a35b32a5a0f934d9e848cbdb
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-02-28 21:11:20 -0500

    Update Point.java

--- 01-point/Point.java
+++ 01-point/Point.java
@@ -1,1 +1,52 @@
 
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(){
+        this(0, 0);
+    }
+
+    public Point(int x, int y){
+        setLocation(x, y);
+    }
+
+    public double distanceFromOrigin(){
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setLocation(int x, int y){
+
+    }
+
+    public String toString(){
+        return "("+ this.x+","+this.y+")";
+    }
+
+    public void translate(int dx, int dy) {
+        setLocation(x + dx, y + dy);
+    }
+
+    public void Distance(Object Point1, Object Point2){
+
+    }
+
+    public void midPoint(Object Point1, Object Point2){
+
+    }
+
+    public void slope(Object Point1, Object Point2){
+
+    }
+    public void isCollinear(Object Point1, Object Point2){
+
+    }
+}
