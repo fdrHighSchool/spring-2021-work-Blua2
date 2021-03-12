0b4e7a07af45de140b90feebb6d23aff5948468f
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-03-12 07:01:41 -0500

    Update DriverArray.java

--- 00-SuperArray/DriverArray.java
+++ 00-SuperArray/DriverArray.java
@@ -1,8 +1,8 @@
 public class DriverArray {
     public static void main(String[] args){
-        SuperArray Array1 = new SuperArray();
-        Array1.SuperArr(15);
-
+        SuperArray Array1 = new SuperArray(15);
+        SuperMMMR SArray = new SuperMMMR();
+        
         Array1.isEmpty();
         Array1.add(0, 12);
         Array1.add(1,7);
@@ -15,5 +15,18 @@ public class DriverArray {
         Array1.grow(5);
         Array1.toString();
         Array1.add(21);
+
+        SArray.add(1);
+        SArray.add(12);
+        SArray.add(18);
+        SArray.add(21);
+        SArray.add(14);
+        SArray.add(6);
+        SArray.add(8);
+        SArray.Average();
+        SArray.Mean();
+        SArray.Median();
+        SArray.Mode();
+        SArray.Range();
     }
 }
