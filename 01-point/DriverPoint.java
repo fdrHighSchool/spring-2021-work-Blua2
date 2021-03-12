5e90a0945e832614f885cb245f4fd7314c5f91ce
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-03-11 22:36:30 -0500

    Update DriverPoint.java

--- 01-point/DriverPoint.java
+++ 01-point/DriverPoint.java
@@ -4,13 +4,18 @@ public class DriverPoint{
     public static void main(String[] args){
        ArrayList<Object> Points = new ArrayList<Object>();
 
-       Point p1 = new Point(5, 6);
-       Point p2 = new Point(3, 3);
+       Point Point1 = new Point(5, 4);
+       Point Point2 = new Point(2, 2);
+       Point Point3 = new Point(3, 3);
         
-       Points.add(p1);
-       Points.add(p2);
+       Points.add(Point1);
+       Points.add(Point2);
+       Points.add(Point3);
 
-       System.out.println(p1 + "  " +  p2);
        System.out.println(Points);
+       System.out.println("Distance: " + Point1.Distance(Point1, Point2));
+       System.out.println("Midpoint: " + Point1.midPoint(Point1, Point2));
+       System.out.println("Slope: " + Point1.slope(Point1, Point2));
+       System.out.println("Collinear: " + Point1.isCollinear(Point1, Point2, Point3));
     }
 }
