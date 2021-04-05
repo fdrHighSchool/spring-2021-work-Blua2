2858dccb251b04fb3b049e3053a4a1cf30aec529
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-04-04 21:41:00 -0400

    Update Customer.java

--- 02 - inheritance/Customer.java
+++ 02 - inheritance/Customer.java
@@ -23,17 +23,11 @@ public class Customer{
       Customer c = new Customer("Fran Santiago", "123 Main St., Anytown, USA");
       System.out.println(c);
 
-      // Uncomment these to test OnlineCustomer
-      // OnlineCustomer c2 = new OnlineCustomer("Jasper Smith", "456 High St., Anytown, USA", "jsmith456@gmail.com");
-      // System.out.println(c2);
+       OnlineCustomer c2 = new OnlineCustomer("Jasper Smith", "456 High St., Anytown, USA", "jsmith456@gmail.com");
+       System.out.println(c2);
    }
 }
 
-// Complete the OnlineCustomer class to inherit from Customer
-// It should have an email attribute,
-// a constructor with 3 arguments (name, address, email) that uses the super constructor,
-// and an overriden toString() method that calls the super toString() method
-//  and then prints "\nEmail:" and the email variable.
 class OnlineCustomer extends Customer{
    private String email;
 
