5b6faac694a84a7a4cc4e963782a5cf71b95c516
Blua2 <72803885+Blua2@users.noreply.github.com>
2021-04-09 03:57:08 -0400

    Add files via upload

---
+++ 04 - Shapes_Inheritance/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends Ellipse{
+    private double radius;
+
+    public Circle(double radius){
+        super("Circle", radius, radius);
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+}
